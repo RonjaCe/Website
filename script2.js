3f779134dee52e1a278079c77ffeb47f396d531e
LouisaMac <127091815+LouisaMac@users.noreply.github.com>
2023-03-06 11:01:33 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+function neuerTermin(){
+    
+    document.getElementById("terminemo").innerHTML ="neuer Text";
+    window.alert(document.getElementById("terminemo").innerHTML);
+
+        };
+
+        Event.preventDefault();
+        
