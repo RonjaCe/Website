4d709fdb531f2ccf019e442ff12ed04c1b8581c9
LouisaMac <127091815+LouisaMac@users.noreply.github.com>
2023-03-09 10:07:15 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,62 @@
+function neuerTerminMo(){
+    
+    document.getElementById("terminemo").innerHTML ="neuer Text";
+    
+
+        };
+
+        Event.preventDefault();
+       
+ function neuerTerminDi(){
+    
+     document.getElementById("terminedi").innerHTML ="neuer Text";
+            
+        
+         };
+        
+         Event.preventDefault();
+
+function neuerTerminMi(){
+    
+    document.getElementById("terminemi").innerHTML ="neuer Text";
+                   
+               
+         };
+               
+          Event.preventDefault();
+
+ function neuerTerminDo(){
+    
+     document.getElementById("terminedo").innerHTML ="neuer Text";
+                           
+                       
+            };
+                       
+           Event.preventDefault();
+        
+function neuerTerminFr(){
+    
+     document.getElementById("terminefr").innerHTML ="neuer Text";
+                           
+                       
+              };
+                       
+              Event.preventDefault();
+
+function neuerTerminSa(){
+    
+     document.getElementById("terminesa").innerHTML ="neuer Text";
+                               
+                           
+              };
+                           
+              Event.preventDefault();    
+              
+function neuerTerminSo(){
+    
+    document.getElementById("termineso").innerHTML ="neuer Text";
+                               
+                           
+              };
+                           
+                Event.preventDefault();
