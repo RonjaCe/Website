bcdfffa7072a1597b3bfcd5e13a55d5a4153fd0b
RonjaCe <127091583+RonjaCe@users.noreply.github.com>
2023-03-09 10:07:26 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,283 @@
+function neuerTerminWo1() {
+  document.getElementById("woche1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMo1() {
+  document.getElementById("montag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDi1() {
+  document.getElementById("dienstag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMi1() {
+  document.getElementById("mittwoch1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDo1() {
+  document.getElementById("donnerstag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminFr1() {
+  document.getElementById("freitag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSa1() {
+  document.getElementById("samstag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSo1() {
+  document.getElementById("sontag1").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+
+function neuerTerminWo2() {
+  document.getElementById("woche2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMo2() {
+  document.getElementById("montag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDi2() {
+  document.getElementById("dienstag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMi2() {
+  document.getElementById("mittwoch2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDo2() {
+  document.getElementById("donnerstag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminFr2() {
+  document.getElementById("freitag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSa2() {
+  document.getElementById("samstag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSo2() {
+  document.getElementById("sontag2").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+
+
+function neuerTerminWo3() {
+  document.getElementById("woche3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMo3() {
+  document.getElementById("montag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDi3() {
+  document.getElementById("dienstag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMi3() {
+  document.getElementById("mittwoch3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDo3() {
+  document.getElementById("donnerstag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminFr3() {
+  document.getElementById("freitag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSa3() {
+  document.getElementById("samstag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSo3() {
+  document.getElementById("sontag3").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+
+function neuerTerminWo4() {
+  document.getElementById("woche4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMo4() {
+  document.getElementById("montag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDi4() {
+  document.getElementById("dienstag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMi4() {
+  document.getElementById("mittwoch4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDo4() {
+  document.getElementById("donnerstag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminFr4() {
+  document.getElementById("freitag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSa4() {
+  document.getElementById("samstag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSo4() {
+  document.getElementById("sontag4").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+
+function neuerTerminWo5() {
+  document.getElementById("woche5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMo5() {
+  document.getElementById("montag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDi5() {
+  document.getElementById("dienstag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminMi5() {
+  document.getElementById("mittwoch5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminDo5() {
+  document.getElementById("donnerstag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminFr5() {
+  document.getElementById("freitag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSa5() {
+  document.getElementById("samstag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
+
+
+function neuerTerminSo5() {
+  document.getElementById("sontag5").innerHTML="Hallo";
+}
+
+Event.preventDefault();
